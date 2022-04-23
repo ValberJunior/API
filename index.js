28b892c55eca23141455fe76248abc123adbd8ec
ValberJunior <valberjr1992@gmail.com>
2022-04-23 03:40:27 -0300

    changes

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const whiteList = {origin: "*" };
 app.use(cors(whiteList));
 
 app.use("/done",express.static(path.join(__dirname,"client")));
-app.use("alunos/done",express.static(path.join(__dirname,"client")));
+
 app.use("/delete",express.static(path.join(__dirname,"client/delete.html")));
 
 
