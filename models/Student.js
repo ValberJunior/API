8f2c594640c6cb9c03d8119765e1649bce9fb3f1
ValberJunior <valberjr1992@gmail.com>
2022-04-25 11:53:06 -0300

    update

--- models/Student.js
+++ models/Student.js
@@ -17,7 +17,7 @@ estado: { type: String, required: true, minlength: 2, maxlength: 2 },
 turma: { type: String, required: true, minlength: 1, maxlength: 20 },
 funcao: { type: String, required: true, minlength: 3, maxlength: 100 },
 matricula: { type: String, required: true, minlength: 4, maxlength: 15 },
-comentarios: { type: String, required: false, minlength: 3, maxlength: 500 },
+comentarios: { type: String, required: false, minlength: 0, maxlength: 500 },
 createAt: { type: Date, default: Date.now },
 });
 
