c2b7481e5bf2ff37058d781351fa22b78c9ac6a0
ValberJunior <valberjr1992@gmail.com>
2022-04-25 11:20:54 -0300

    update

--- controllers/studentControler.js
+++ controllers/studentControler.js
@@ -73,8 +73,7 @@ const studentController = {
         StudentModel.findByIdAndDelete((req.body.id),
         (err,data)=>{
             if(!err){
-                // res.send(data);
-                res.redirect("/delete");
+                res.status(200).send(data);
             }else{
                res.status(404).send("Invalid Data");
             }
