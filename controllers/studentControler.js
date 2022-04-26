e5158ac8cf4841ca17655fe23734825bdde28fe7
ValberJunior <valberjr1992@gmail.com>
2022-04-26 13:00:52 -0300

    update

--- controllers/studentControler.js
+++ controllers/studentControler.js
@@ -78,7 +78,7 @@ const studentController = {
             funcao: req.body.funcao,
             matricula: req.body.matricula,
             comentarios: req.body.comentarios,
-            imagem: req.body.imagem ,
+            imagem: req.body.imagem || "https://media.istockphoto.com/vectors/default-placeholder-profile-icon-vector-id666545204?b=1&k=20&m=666545204&s=612x612&w=0&h=lISapC3Y2nnRS1hC0dZbTl0iHLL28JqmJJnie4JygAA=" ,
  
             };
 
